908eed066371334b10cc6e006a3063fec5f31285
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-20 16:27:11 +0200

    Lesson 008-10

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -10,21 +10,30 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
+        NavigationView {
         Form {
             Section {
                 TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                     .keyboardType(.decimalPad)
+                
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2 ..< 100) {
+                        Text("\($0) people")
+                    }
+                }
             }
             
             Section {
                 Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
             }
+        }
+        .navigationTitle("WeSplit")
         }
     }
 }
