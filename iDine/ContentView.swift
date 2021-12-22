cb730881b0c42db9094a6351515072b2e7d960c6
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-22 20:31:56 +0200

    ol-011-10

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -9,19 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var checkAmount = ""
+    @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
-    @State private var tipPercentage = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var totalPerPerson: Double {
-       let peopleCount = Double(numberOfPeople + 2)
+        let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentage)
-        let orderAmount = Double(checkAmount) ?? 0
         
-        let tipValue = orderAmount / 100 * tipSelection
-        let grandTotal = orderAmount + tipValue
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
         
         return amountPerPerson
@@ -31,10 +31,11 @@ struct ContentView: View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Amount", text: $checkAmount)
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
-                    Picker("Number of people", selection: $numberOfPeople) {
+                    Picker("Number of peoplt", selection: $numberOfPeople) {
                         ForEach(2 ..< 100) {
                             Text("\($0) people")
                         }
@@ -50,18 +51,81 @@ struct ContentView: View {
                     .pickerStyle(.segmented)
                 } header: {
                     Text("How much tip do you want to leave?")
-                        .textCase(nil)
                 }
-            
                 
                 Section {
-                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
+        
     }
 }
+
+//struct ContentView: View {
+//
+//    @State private var checkAmount = ""
+//    @State private var numberOfPeople = 2
+//    @State private var tipPercentage = 2
+//
+//    let tipPercentages = [10, 15, 20, 25, 0]
+//
+//    var totalPerPerson: Double {
+//       let peopleCount = Double(numberOfPeople + 2)
+//        let tipSelection = Double(tipPercentage)
+//        let orderAmount = Double(checkAmount) ?? 0
+//
+//        let tipValue = orderAmount / 100 * tipSelection
+//        let grandTotal = orderAmount + tipValue
+//        let amountPerPerson = grandTotal / peopleCount
+//
+//        return amountPerPerson
+//    }
+//
+//    var body: some View {
+//        NavigationView {
+//            Form {
+//                Section {
+//                    TextField("Amount", text: $checkAmount)
+//                        .keyboardType(.decimalPad)
+//
+//                    Picker("Number of people", selection: $numberOfPeople) {
+//                        ForEach(2 ..< 100) {
+//                            Text("\($0) people")
+//                        }
+//                    }
+//                }
+//
+//                Section {
+//                    Picker("Tip percentage", selection: $tipPercentage) {
+//                        ForEach(tipPercentages, id: \.self) {
+//                            Text($0, format: .percent)
+//                        }
+//                    }
+//                    .pickerStyle(.segmented)
+//                } header: {
+//                    Text("How much tip do you want to leave?")
+//                        .textCase(nil)
+//                }
+//
+//
+//                Section {
+//                    Text("$\(totalPerPerson, specifier: "%.2f")")
+//                }
+//            }
+//            .navigationTitle("WeSplit")
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
